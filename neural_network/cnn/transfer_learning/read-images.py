1fb61fffd06afa828805eba627dcb6496a7d29a8
hello <hello@eric.local>
2019-08-20 23:40:20 +0800

    add

---
+++ neural_network/cnn/transfer_learning/read-images.py
@@ -0,0 +1,78 @@
+import glob
+import os.path
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+
+
+# directory of input images
+INPUT_DATA = './path/to/flower_data'
+# directory of output images stored in numpy
+OUTPUT_FILE = './path/to/flower_processed_data.npy'
+
+# proportion of validation set
+VALIDATION_PERCENTAGE = 10
+# proportion of test set
+TEST_PERCENTAGE = 10
+
+
+def create_images_lists(sess, testing_percentage, validation_percentage):
+    sub_dirs = [x[0] for x in os.walk(INPUT_DATA)]
+    is_root_dir = True
+    training_images = []
+    training_labels = []
+    testing_images = []
+    testing_labels = []
+    validation_images = []
+    validation_labels = []
+    current_label = 0
+    for sub_dir in sub_dirs:
+        if is_root_dir:
+            is_root_dir = False
+            continue
+        extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
+        file_list = []
+        dir_name = os.path.basename(sub_dir)
+        for extension in extensions:
+            file_glob = os.path.join(INPUT_DATA, dir_name, '*.' + extension)
+            file_list.append(glob.glob(file_glob))
+        if not file_list:
+            continue
+
+        # process images
+        for file_name in file_list: # 299 * 299
+            image_raw_data = gfile.FastGFile(file_name, 'rb').read()
+            image = tf.image.decode_jpeg(contents=image_raw_data)
+            if image.dtype != tf.float32:
+                image = tf.image.convert_image_dtype(image=image, dtype=tf.float32)
+            image = tf.image.resize_images(images=image, size=[299, 299])
+            image_value = sess.run(image)
+
+            chance = np.random.randint(100)
+            if chance < validation_percentage:
+                validation_images.append(image_value)
+                validation_labels.append(current_label)
+            elif chance < (testing_percentage + validation_percentage):
+                testing_images.append(image_value)
+                testing_labels.append(current_label)
+            else:
+                training_images.append(image_value)
+                training_labels.append(current_label)
+        current_label += 1
+
+    # shuffle data
+    state = np.random.get_state()
+    np.random.shuffle(testing_images)
+    np.random.set_state(state)
+    np.random.shuffle(testing_labels)
+
+    return np.asarray([training_images, training_labels, validation_images, validation_labels, testing_images, testing_labels])
+
+
+def main():
+    with tf.Session() as sess:
+        processed_data = create_images_lists(sess, TEST_PERCENTAGE, VALIDATION_PERCENTAGE)
+        np.save(OUTPUT_FILE, processed_data)
+
+if __name__ == '__main__':
+    main()
---
+++ neural_network/cnn/transfer_learning/train-model.py
@@ -0,0 +1,100 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import tensorflow.contrib.slim.python.slim.nets.inception_v3 as inception_v3
+
+# wget http://download.tensorflow.org/example_images/flower_photos.tgz
+# wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+# directory of processed images
+INPUT_DATA = './path/to/flower_processed_data.npy'
+# train path
+TRAIN_PATH = './path/to/save_model'
+# model path
+CKPT_FILE = './path/to/inception_v3.ckpt'
+
+LEARNING_RATE = 0.01
+STEPS = 4000
+BATCH = 32
+N_CLASS = 5
+
+CHECKPOINT_EXCLUDE_SCOPES = 'InceptionV3/Logits,InceptionV3/AuxLogits'
+TRAINABLE_SCOPES = 'InceptionV3/Logits,InceptionV3/AuxLogits'
+
+
+def get_tuned_variables():
+    exclusions = [scope.strip() for scope in CHECKPOINT_EXCLUDE_SCOPES.split(',')]
+    variables_to_restore = []
+    for var in slim.get_model_variables():
+        excluded = False
+        for exclusion in exclusions:
+            if var.op.name.startswith(exclusion):
+                excluded = True
+                break
+        if not excluded:
+            variables_to_restore.append(var)
+    return variables_to_restore
+
+
+def get_trainable_variables():
+    scopes = [scope.strip() for scope in TRAINABLE_SCOPES.split(',')]
+    variables_to_train = []
+    for scope in scopes:
+        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+        variables_to_train.append(variables)
+
+
+def main():
+    processed_data = np.load(INPUT_DATA)
+    training_images = processed_data[0]
+    n_training_examples = len(training_images)
+    training_labels = processed_data[1]
+    validation_images = processed_data[2]
+    validation_labels = processed_data[3]
+    testing_images = processed_data[4]
+    testing_labels = processed_data[5]
+
+    images = tf.placeholder(tf.float32, [None, 299, 299, 3], name='input_images')
+    labels = tf.placeholder(tf.int64, [None], name='labels')
+
+    with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
+        logits, _ = inception_v3.inception_v3(inputs=images, num_classes=N_CLASS)
+
+    trainable_variables = get_trainable_variables()
+    tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(labels, N_CLASS), logits=logits, weights=1.0)
+    train_step = tf.train.RMSPropOptimizer(LEARNING_RATE).minimize(tf.losses.get_total_loss())
+
+    with tf.name_scope('evaluation'):
+        correct_prediction = tf.equal(tf.argmax(tf.argmax(logits, 1), labels))
+        evaluation_step = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # define function to load model
+    load_fn = slim.assign_from_checkpoint_fn(model_path=CKPT_FILE, var_list=get_tuned_variables(), ignore_missing_vars=True)
+
+    saver = tf.train.Saver(max_to_keep=100)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print('loading model')
+        sess.run(load_fn)
+
+        start = 0
+        end = BATCH
+        for i in range(STEPS):
+            sess.run(train_step, feed_dict={images: training_images[start:end], labels: training_labels[start,end]})
+            if (i + 1) % 30 == 0 or i + 1 == STEPS:
+                saver.save(sess=sess, save_path=TRAIN_PATH, global_step=i)
+                validation_acc = sess.run(evaluation_step, feed_dict={images: validation_images, labels: validation_labels})
+                print()
+            start = end
+            if start == n_training_examples:
+                start = 0
+            end = start + BATCH
+            if end > n_training_examples:
+                end = n_training_examples
+
+        test_acc = sess.run(evaluation_step, feed_dict={images: testing_images, labels: testing_labels})
+        print()
+
+
+if __name__ == '__main__':
+    main()
